1c471e8416fd70ffcf6fdaff88f96024dd149086
pkch93 <star_final@naver.com>
2020-04-23 20:17:20 +0900

    TIL | 20200423 0817 PM | 오브젝트 1장

---
+++ object-practice/chapter01/src/main/java/edu/pkch/objectpractice/domain/Invitation.java
@@ -0,0 +1,7 @@
+package edu.pkch.objectpractice.domain;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ object-practice/chapter01/src/main/java/edu/pkch/objectpractice/domain/Ticket.java
@@ -0,0 +1,9 @@
+package edu.pkch.objectpractice.domain;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
