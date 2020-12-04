db7699152b7713a3c4b485e6e5cecd20c7f62e23
pkch93 <star_final@naver.com>
2020-12-04 17:00:38 +0900

    TIL | 201204 0500 PM | 스프링부트 이미지 빌드 지원 학습 프로젝트 생성

---
+++ spring_build_edu/src/main/java/edu/pkch/build/controller/HelloController.java
@@ -0,0 +1,13 @@
+package edu.pkch.build.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
